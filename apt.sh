36e141f6ee7b64c66b4aba56375fa930802ceca4
a114tw123 <a114tw123@gmail.com>
2020-06-24 18:28:31 +0800

    1

--- apt.sh
+++ apt.sh
@@ -1,4 +1,4 @@
-﻿apt install xorg icewm vim net-tools gcin libappindicator3-dev fonts-noto bridge-utils gcc cpp g++ gcc-arm-linux-gnueabihf cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf u-boot-tools qemu-system:armhf qemu-user:armhf qemu-user-static debootstrap rsync -y
+﻿apt install xorg icewm vim net-tools gcin libappindicator3-dev fonts-noto bridge-utils gcc cpp g++ gcc-arm-linux-gnueabihf cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf u-boot-tools qemu-system-armhf qemu-user-static debootstrap rsync -y
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #dpkg -i ./google-chrome-stable_current_amd64.deb
 #apt install -f
