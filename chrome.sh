6b2a8dcd18e8c85f7b90aee1f048c08686686c53
a114tw123 <51195072+a114tw123@users.noreply.github.com>
2020-06-24 19:00:15 +0800

    Add files via upload

--- chrome.sh
+++ chrome.sh
@@ -1,11 +1,11 @@
 ﻿
-dpkg --add-architecture armhf
-apt update
-apt install xorg icewm vim net-tools gcin libappindicator3-dev \
+#dpkg --add-architecture armhf
+#apt update
+#apt install xorg icewm vim net-tools gcin libappindicator3-dev \
        	fonts-noto bridge-utils gcc cpp g++ gcc-arm-linux-gnueabihf \
        	cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf u-boot-tools \
         qemu qemu-kvm qemu-system:armhf qemu-user:armhf qemu-user-static \
       	debootstrap rsync git nautilus -y
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#dpkg -i ./google-chrome-stable_current_amd64.deb
-#apt install -f
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i ./google-chrome-stable_current_amd64.deb
+apt install -f
