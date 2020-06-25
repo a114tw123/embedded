9c05f2b675d0159b1a58c14f1da6df784fc1842c
a114tw123 <51195072+a114tw123@users.noreply.github.com>
2020-06-25 14:24:25 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -5,5 +5,5 @@ apt install xorg icewm vim net-tools gcin libappindicator3-dev \
        	fonts-noto bridge-utils gcc cpp g++ gcc-arm-linux-gnueabihf \
        	cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf u-boot-tools \
         qemu qemu-kvm qemu-system:armhf qemu-user:armhf qemu-user-static \
-      	debootstrap rsync git nautilus libncureses5-dev -y
+      	debootstrap rsync git nautilus libncurses5-dev -y
 git clone https://github.com/a114tw123/embedded.git
