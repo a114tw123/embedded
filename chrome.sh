76d8791af6b1cd0d7ad23eeebb196d49fd94703d
a114tw123 <51195072+a114tw123@users.noreply.github.com>
2020-06-25 14:06:31 +0800

    Add files via upload

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,6 @@
 ﻿
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#apt install ./google-chrome-stable_current_amd64.deb -y
+apt install ./google-chrome-stable_current_amd64.deb -y
 
 wget http://ftp.tw.debian.org/debian/pool/main/k/kernel-package/kernel-package_13.014+nmu1_all.deb
 apt install ./kernel-package_13.014+nmu1_all.deb -y
